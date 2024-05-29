c8375a77a59964bb0b2aa184039caa506507621c
wellapos <wellington258@live.com>
2024-05-28 23:51:02 -0300

    feat: teste husky pre-commit

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -8,6 +8,7 @@ import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
 import { it } from '@jest/globals';
+// test
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
